83ebfd5f446adbc658fc883d42d78ec27dd15a08
gustateixeira <gustavohteixeira2904@gmail.com>
2024-11-11 20:30:00 -0300

    mudança nas funções

--- src/main.hs
+++ src/main.hs
@@ -15,87 +15,151 @@ data CPU = CPU{
     pc :: Reg,
     memory :: Memory,
     ir :: (Reg,Reg)
-}
-
---LOD
-execLOD :: Int -> (Memory, Reg, Bool) -> (Memory, Int, Bool)
-execLOD end (mem, acc, eqz) = (mem, readMem mem end, eqz)
+}deriving Show
 
 
 
---JMP
--- readMem [(0,10),(1,3),(2,23),(10,100)] 1 = 3
--- fst -> primeiro elemento da tupla
+{-Escrita da memória-}
+writeMem :: Memory -> Int -> Reg -> Memory
+writeMem (m:ms) end acc | end == fst m = [(fst m, acc)] ++ ms
+                        | otherwise = m : writeMem ms end acc
 
--- snd -> segundo elemento da tupla
+{-Leitura da memória-}
 readMem :: Memory -> Int -> Int
 readMem (m:ms) end  
     | end == fst m = snd m
-    | end /= fst m = readMem ms end    
+    | otherwise = readMem ms end    
 
 
---Escrita da memória
-execSto :: Memory -> Int -> Reg -> Memory
-execSto (m:ms) end acc | end == fst m = [(fst m, acc)] ++ ms
-                       | end /= fst m = m : execSto ms end acc
+{-Carrega o conteúdo do endereço de memória <end> no
+registrador acumulador (ACC).-}
+
+execLOD :: CPU ->  CPU
+execLOD cpu = cpu {ula = (ula cpu){acc = value} , pc = pc cpu + 2}
+        where value = readMem (memory cpu) (snd(ir cpu))
+
+{-
+Armazena o conteúdo do registrador acumulador (ACC) no
+endereço de memória <end>
+-}
+execSto :: CPU ->  CPU
+execSto cpu = cpu{ memory = newmemory, pc = pc cpu + 2}
+        where newmemory = writeMem (memory cpu) (snd(ir cpu)) (acc(ula cpu))
+
+{-
+Desvio incondicional: carrega no contador de instruções (PC) o valor
+<end> forçando com que a próxima instrução a ser executada
+seja a que se encontra no endereço de memória <end>
+-}
+
+execJmp :: CPU -> CPU
+execJmp cpu = cpu{pc = end}
+            where end = (snd(ir cpu))
 
 
-execJmp :: Int -> Int ->Int
-execJmp end pc = if pc /= end then execJmp end (pc+2) 
-                else pc 
-      
--- [(Instruções, funções)] -> retorna função
+{-
+Desvio condicional: funcionamento análogo ao da instrução JMP
+com a diferença que a carga do contador de instruções só ocorre
+se o valor do acumulador for igual a zero (de acordo com a flag
+EQZ)
 
-execJmz :: Int -> Int -> Bool -> Int
 execJmz end pc eqz = if pc == end && eqz == True then end 
                 else pc
 
-execCpe :: Int -> Int -> Int
-execCpe end acc = if acc == end then  0
+-}
+
+execJmz :: CPU -> CPU
+execJmz cpu = if pc cpu == end && eqz(ula cpu) == True then cpu{pc = end} else cpu{pc = pc cpu + 2}
+                where end = readMem (memory cpu) (snd(ir cpu))
+
+
+{-
+Se o conteúdo do endereço <end> for igual ao acumulador,
+coloca 0 no acumulador, caso contrário coloca 1
+
+end acc = if acc == end then  0
                 else 1
 
-execAdd :: Int -> Int -> Int 
-execAdd end acc = if (acc+end) < 127 && (acc+end) > (-128)  then (acc+end)
-                 else (-128)
+-}
 
-execSub :: Int -> Int -> Int 
+execCpe :: CPU -> CPU
+execCpe cpu = if acc(ula cpu) == end then cpu{ pc = pc cpu + 2, ula = (ula cpu) {acc = 0}} 
+            else cpu {ula = (ula cpu) {acc = 1}} 
+            where end = readMem (memory cpu) (snd(ir cpu))
+
+{-
+Adiciona o conteúdo do endereço de memória <end> ao
+conteúdo armazenado no acumulador (ACC) e armazena a
+resposta no próprio acumulador
+-}
+--execAdd :: CPU -> CPU
+--execAdd cpu = if (acc+end) < 127 && (acc+end) > (-128)  then (acc+end)
+  --               else (-128)
+
+{-
+Subtrai o conteúdo do endereço de memória <end> do conteúdo
+do acumulador (ACC) e armazena a resposta no próprio
+acumulador
+-}
+
+execSub :: Int -> Reg -> Int 
 execSub end acc = if (acc-end) < 127 && (acc-end) > (-128)  then (acc-end)
                  else (-128)
+
+{-
+Não executa ação nenhuma (No OPeration)
+-}
 
 execNop :: Int
 execNop = 0
 
-execHlt :: String 
-execHlt = "Programa encerrado."
+{-
+Encerra o ciclo de execução do processador (HaLT)
+-}
+execHlt :: Int 
+execHlt = -1
 
 
+createMemory :: Memory
+createMemory = zip [0..255] (repeat 0)
 
-setAcc :: (Int->Int->Int)-> Int-> Int-> Int
-setAcc f end acc= f end acc   
+createUla :: ULA
+createUla = ULA{
+    acc = 0,
+    eqz = True
+}
 
 
+createCpu :: CPU
+createCpu = CPU{
+    ula = createUla,
+    pc = 0,
+    memory = createMemory,
+    ir = (0,1)
+}
 
-exec :: Reg -> Bool -> Memory -> Memory 
-exec acc eqz (m:ms)  | snd m == 2 = let (mem,acc , _) = execLOD (snd (head(ms))) ([m]++ms,acc,eqz)
-                            in mem
-        
---[(0,2), (1,240)]
--- (m:ms)
--- m = (0,2)
--- ms = [(1,240)]
+finalMemory :: CPU -> Memory
+finalMemory cpu = memory cpu
 
+{-exec :: CPU -> CPU
+exec cpu = case fst (ir cpu) of
+    2  -> lod cpu
+    4  -> sto cpu
+    6  -> jmp cpu
+    8  -> jmz cpu
+    10 -> cpe cpu
+    14 -> add cpu
+    16 -> sub cpu
+    18 -> nop cpu
+    20 -> hlt cpu
+-}
 main  :: IO()
 
 --acc = posição 100
 
 main = do
-    let mem = [(0,2),(1,240),(2,14),(3,241),(4,4),(5,251),(6,20),(7,18),(240,10),(241,1),(251,0)]
-    let acc = 0
-    let eqz = True
-    let newMem = exec acc eqz mem
-    print acc
-    print newMem
-
+    let c = createCpu
+    print c
 
 
 
